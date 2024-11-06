dd8e92393891e4baff3d7a56dc59589d9ab475fe
uv-deva <yuvaraj.d@webllisto.com>
2024-11-06 19:25:40 +0530

    ticker, candles and 24h api added

--- controller/contract.js
+++ controller/contract.js
@@ -2,14 +2,47 @@ const ethers = require('ethers');
 require('dotenv').config();
 const abi = require('../abi/priceFeed.json');
 const tokens = require('./tokenList.json')
+const axios = require('axios');
 
-const { INFURA_URL, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
+const { INFURA_URL, PRIVATE_KEY, CONTRACT_ADDRESS, SUBGRAPH_API_URLS } = process.env;
 
 const provider = new ethers.JsonRpcProvider(INFURA_URL);
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
 
-const getTokenDetails = async () => {
+const priceCandleQuery = `
+  query GetPriceCandles($first: Int, $period: String) {
+    priceCandles(first: $first, where: { period: $period }) {
+      id
+      token
+      open
+      high
+      low
+      close
+      timestamp
+      period
+    }
+  }
+`;
+
+async function fetchGraphQL(endpoint, query, variables = {}) {
+    // const headers = {
+    //   'Content-Type': 'application/json',
+    // };
+    try {
+      const response = await axios.post(endpoint, {
+        query,
+        variables,
+      });
+  
+    //   console.log(JSON.stringify(response.data, null, 2));
+      return response.data;
+    } catch (error) {
+      console.error('Error calling GraphQL API:', error);
+    }
+  }
+
+const getTickers = async () => {
     try {
         let price= [];
         for(let i = 0; i < tokens.length; i++) {
@@ -34,7 +67,60 @@ const getTokenDetails = async () => {
         throw new Error(`Error fetching token details: ${error.message}`);
     }
 };
+        
+const get24hData = async (limit) => {
+    try {
+        let candlesData = [];
+        const endpoint = SUBGRAPH_API_URLS;
+        const data = await fetchGraphQL(endpoint, priceCandleQuery, { first: parseInt(limit), period: "1d" })
+        for(let i = 0; i < data.data.priceCandles.length; i++) {
+            let tokenData = await contract.tokenPriceData(data.data.priceCandles[i].token);
+            candlesData.push({
+                "tokenSymbol": tokenData[0],
+                "high": data.data.priceCandles[i].high,
+                "low": data.data.priceCandles[i].low,
+                "open": data.data.priceCandles[i].open,
+                "close": data.data.priceCandles[i].close
+            })
+        }
+        console.log(candlesData)
+        return candlesData;
+    } catch (error) {
+        throw new Error(`Error fetching token details: ${error.message}`);
+    }
+};
+
+const getCandles = async (limit, period) => {
+    try {
+        let candlesData = [];
+        const endpoint = SUBGRAPH_API_URLS;
+        const data = await fetchGraphQL(endpoint, priceCandleQuery, { first: parseInt(limit), period })
+        for(let i = 0; i < limit; i++) {
+            candlesData.push({
+                'period': period,
+                "candles": [[
+                    data.data.priceCandles[i].timestamp,
+                    data.data.priceCandles[i].open,
+                    data.data.priceCandles[i].high,
+                    data.data.priceCandles[i].low,
+                    data.data.priceCandles[i].close
+                ]]
+            })
+        }
+        return candlesData;
+    } catch (error) {
+        throw new Error(`Error fetching token details: ${error.message}`);
+    }
+};
+
+const getIncentives = async () => {
+    try {
+
+    } catch (error) {
+        throw new Error(`Error fetching token details: ${error.message}`);
+    }
+};
 
 module.exports = {
-    getTokenDetails
+    getTickers, get24hData, getCandles, getIncentives
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors'); 
-const { getTokenDetails } = require('../controller/contract');  // Import contract functions
+const { getTickers, get24hData, getCandles, getIncentives } = require('../controller/contract');  // Import contract functions
 const logger = require('../logger');  // Import logger
 require('dotenv').config();
 const app = express();
@@ -9,9 +9,49 @@ app.use(cors());
 
 const { PORT } = process.env;
 
-app.get('/token-price', async (req, res) => {
+// app.get('/token-price', async (req, res) => {
+//     try {
+//         const details = await getTokenDetails();
+//         res.json(details.price);
+//     } catch (error) {
+//         logger.error(`Error fetching token details: ${error.message}`);
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+app.get('/prices/tickers', async (req, res) => {
     try {
-        const details = await getTokenDetails();
+        const details = await getTickers();
+        res.json(details.price);
+    } catch (error) {
+        logger.error(`Error fetching token details: ${error.message}`);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/prices/24h', async (req, res) => {
+    try {
+        const details = await get24hData(req.query.limit);
+        res.json(details);
+    } catch (error) {
+        logger.error(`Error fetching token prices 24h details: ${error.message}`);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/prices/candles', async (req, res) => {
+    try {
+        const details = await getCandles(req.query.limit, req.query.period);
+        res.json(details);
+    } catch (error) {
+        logger.error(`Error fetching token details: ${error.message}`);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/prices/incentives', async (req, res) => {
+    try {
+        const details = await getIncentives();
         res.json(details.price);
     } catch (error) {
         logger.error(`Error fetching token details: ${error.message}`);
